034b7d010f1649138358256bf9d90b030721cebf
Mo Zhang <mzhang@microsoft.com>
2019-12-11 17:26:24 +0100

    cameraCallCv

--- cameraCallCv/index.js
+++ cameraCallCv/index.js
@@ -1,16 +1,36 @@
-module.exports = async function (context, req) {
+//
+// Multi purpose "camera" Function.
+// - Serves the camera single page "mobile app" (static HTML file)
+// - Receives images from the camera app and saves them to blob storage 
+// Ben Coleman, Aug 2018
+//
+module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+  
+    // If we get receive a GET, lets serve up a page of static HTML
+    // This page is the HTML5/JS camera mini "app" that will POST photos back to this Function in Base64 format
+    // NOTE. Serving static content like this from a Function, is a really hacky thing to do! Not best practice!
+    if (req.method == 'GET') {
+      context.log("### User requested camera app web page");
+  
+      // Look I told you this was a hack! I feel bad about this code now
+      var cameraHtml = require('fs').readFileSync(__dirname+'/cameraCallCv.html').toString();
+      context.res.status = 200;
+      context.res.headers['Content-Type'] = "text/html";
+      context.res.body = cameraHtml;
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+  
+    // If we get a POST - it's an image being uploaded from the camera app
+    // So... Base64 decode it and write to the outputBlob stream 
+    if (req.method == 'POST') {
+      context.log("### New image/photo uploaded, storing as blob");
+      var buffer = new Buffer.from(req.body.toString(), 'base64');
+      context.bindings.outputBlob = buffer;
+      context.res.status = 200;
+      context.res.body = "";
     }
-};
+  
+    context.log("### Function complete");
+    context.done();
+  };
+  
